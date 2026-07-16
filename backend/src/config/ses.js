068454ebed5b9e6da057688dd62852e0105eb8d2
Seth Kelvin <sethkelvina@gmail.com>
2026-07-16 06:51:15 +0000

    Use IAM role credentials for AWS SDK

--- backend/src/config/ses.js
+++ backend/src/config/ses.js
@@ -1,26 +1,34 @@
-const { SESClient, SendEmailCommand } = require('@aws-sdk/client-ses');
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 
 const ses = new SESClient({
-  region: process.env.AWS_REGION || 'us-east-1',
-  credentials: {
-    accessKeyId:     process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+  region: process.env.AWS_REGION || "us-east-1",
 });
 
 async function sendEmail({ to, subject, html, text }) {
   const command = new SendEmailCommand({
     Source: process.env.SES_FROM_EMAIL,
-    Destination: { ToAddresses: Array.isArray(to) ? to : [to] },
+    Destination: {
+      ToAddresses: Array.isArray(to) ? to : [to],
+    },
     Message: {
-      Subject: { Data: subject },
+      Subject: {
+        Data: subject,
+      },
       Body: {
-        Html: { Data: html },
-        Text: { Data: text || html.replace(/<[^>]+>/g, '') },
+        Html: {
+          Data: html,
+        },
+        Text: {
+          Data: text || html.replace(/<[^>]+>/g, ""),
+        },
       },
     },
   });
+
   return ses.send(command);
 }
 
-module.exports = { ses, sendEmail };
+module.exports = {
+  ses,
+  sendEmail,
+};
